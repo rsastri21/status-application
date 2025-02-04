a2610f567763da1b561b70e2c45411a6e9841c96
rsastri21 <rsastri21@gmail.com>
2025-02-03 19:48:59 -0500

    Refactor presigned URL functions to accept additional metadata.

--- packages/core/src/utils/s3-utils.ts
+++ packages/core/src/utils/s3-utils.ts
@@ -1,22 +1,17 @@
 import { HeadObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3ClientProvider } from "./s3-client";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { HEIGHT_METADATA_HEADER, WIDTH_METADATA_HEADER } from "./constants";
 
 export const getUploadPresignedUrl = (
     bucket: string,
     key: string,
-    width: number,
-    height: number
+    metadata: { [key: string]: string }
 ) => {
     const client = S3ClientProvider.getInstance();
     const command = new PutObjectCommand({
         Bucket: bucket,
         Key: key,
-        Metadata: {
-            [WIDTH_METADATA_HEADER]: String(width),
-            [HEIGHT_METADATA_HEADER]: String(height),
-        },
+        Metadata: metadata,
     });
     return getSignedUrl(client, command, { expiresIn: 300 });
 };
--- packages/functions/utils/presigned-url.ts
+++ packages/functions/utils/presigned-url.ts
@@ -1,3 +1,7 @@
+import {
+    HEIGHT_METADATA_HEADER,
+    WIDTH_METADATA_HEADER,
+} from "@status-application/core/utils/constants";
 import { getUploadPresignedUrl } from "@status-application/core/utils/s3-utils";
 import { Resource } from "sst";
 
@@ -6,11 +10,9 @@ export const generateUploadPresignedUrl = async (
     width: number,
     height: number
 ) => {
-    const url = await getUploadPresignedUrl(
-        Resource.Images.name,
-        key,
-        width,
-        height
-    );
+    const url = await getUploadPresignedUrl(Resource.Images.name, key, {
+        [WIDTH_METADATA_HEADER]: String(width),
+        [HEIGHT_METADATA_HEADER]: String(height),
+    });
     return url;
 };
