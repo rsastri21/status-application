7f6861acd2e608cf021e3ae9357354db3b178e4a
rsastri21 <rsastri21@gmail.com>
2025-03-21 21:54:04 -0400

    chore: Refactor auth lambda to use try-catch utility.

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -12,7 +12,9 @@ import { generateSessionToken } from "@status-application/core/utils/auth";
 import {
     createSession,
     deleteSession,
+    SESSION_EXPIRY_TIME,
 } from "@status-application/core/queries/sessions";
+import { tryCatch } from "@status-application/core/utils/try-catch";
 
 const registerSchema = z.object({
     username: z.string().nonempty(),
@@ -33,57 +35,64 @@ const app = new Hono();
  */
 app.put("/api/auth/register", jsonValidator(registerSchema), async (c) => {
     const params = c.req.valid("json");
+    const { data, error } = await tryCatch(
+        createUser(params.username, params.name, params.email, params.password)
+    );
 
-    try {
-        const userRequest = await createUser(
-            params.username,
-            params.name,
-            params.email,
-            params.password
-        );
-        if (userRequest.$metadata.httpStatusCode === 200) {
-            return c.json({ message: "User created." }, 201);
-        }
-        return c.json({ message: "Unsuccessful registration." }, 400);
-    } catch (error) {
-        return c.json({ message: "Unsuccessful creation.", error }, 400);
+    if (error) {
+        return c.json({ message: "Unsuccessful registration.", error }, 400);
     }
+
+    if (data.$metadata.httpStatusCode === 200) {
+        return c.json({ message: "User created." }, 201);
+    }
+    return c.json({ message: "Unsuccessful registration." }, 400);
 });
 
 app.post("/api/auth/sign-in", jsonValidator(signInSchema), async (c) => {
     const params = c.req.valid("json");
 
-    try {
-        const response = await getUserByUsername(params.username);
+    const { data, error } = await tryCatch(getUserByUsername(params.username));
 
-        if (!response.Item) {
-            return c.json({ message: "Account not found." }, 404);
-        }
-
-        const user = response.Item as User;
-        const isPasswordCorrect = await verifyPassword(
-            params.password,
-            user.salt,
-            user.password
-        );
-
-        if (!isPasswordCorrect) {
-            return c.json({ message: "Incorrect login information." }, 401);
-        }
-
-        const token = generateSessionToken();
-        const sessionResponse = await createSession(user.username, token);
-
-        if (sessionResponse.$metadata.httpStatusCode === 200) {
-            return c.json(
-                { message: "Signed-in successfully.", authToken: token },
-                200
-            );
-        }
-        return c.json({ message: "Sign-in unsuccessful." }, 400);
-    } catch (error) {
+    if (error) {
         return c.json({ message: "Sign-in unsuccessful.", error }, 400);
     }
+
+    if (!data.Item) {
+        return c.json({ message: "Account not found." }, 404);
+    }
+
+    const user = data.Item as User;
+
+    const isPasswordCorrect = await verifyPassword(
+        params.password,
+        user.salt,
+        user.password
+    );
+
+    if (!isPasswordCorrect) {
+        return c.json({ message: "Incorrect login information." }, 401);
+    }
+
+    const token = generateSessionToken();
+    const { data: sessionResponse, error: sessionResponseError } =
+        await tryCatch(createSession(user.username, token));
+
+    if (sessionResponseError) {
+        return c.json({ message: "Could not create session.", error }, 400);
+    }
+
+    if (sessionResponse.$metadata.httpStatusCode !== 200) {
+        return c.json({ message: "Sign-in unsuccessful." }, 400);
+    }
+    /**
+     * Return expiry time as half of actual so the same token can be refreshed.
+     */
+    return c.json({
+        message: "Sign-in successful.",
+        authToken: token,
+        expiresAt: Date.now() + SESSION_EXPIRY_TIME / 2,
+    });
 });
 
 app.post("/api/auth/sign-out", async (c) => {
@@ -94,12 +103,12 @@ app.post("/api/auth/sign-out", async (c) => {
         return c.json({ message: "User or token not provided." }, 400);
     }
 
-    try {
-        await deleteSession(username, token);
-        return c.json({ message: "Signed-out successfully." });
-    } catch (error) {
+    const { error } = await tryCatch(deleteSession(username, token));
+
+    if (error) {
         return c.json({ message: "Sign-out failed.", error }, 400);
     }
+    return c.json({ message: "Signed-out successfully." });
 });
 
 export const handler = handle(app);
