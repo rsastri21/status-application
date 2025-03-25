b048b074554734b35d7c370c3be901177c5b9cf8
rsastri21 <rsastri21@gmail.com>
2025-03-24 21:24:34 -0700

    chore: Refactor new post function with tryCatch pattern.

--- packages/functions/src/posts.ts
+++ packages/functions/src/posts.ts
@@ -9,6 +9,7 @@ import {
 import { generatePostPresignedUrls } from "../utils/presigned-url";
 import { getMidnightEpoch } from "../utils/get-midnight-date";
 import { DAILY_POST_LIMIT } from "@status-application/core/utils/constants";
+import { tryCatch } from "@status-application/core/utils/try-catch";
 
 const postSchema = z.object({
     width: z.number().nonnegative(),
@@ -25,50 +26,67 @@ app.post("/api/posts/new", jsonValidator(postSchema), async (c) => {
      * Validate that the user has not exceeded the
      * daily post limit before creating a post.
      */
-    try {
-        const currentDayMidnight = getMidnightEpoch();
+    const currentDayMidnight = getMidnightEpoch();
+    const { data, error } = await tryCatch(
+        getPostsForUserWithinRange(username, currentDayMidnight)
+    );
 
-        const response = await getPostsForUserWithinRange(
-            username,
-            currentDayMidnight
-        );
-        if (response.Count && response.Count > DAILY_POST_LIMIT) {
-            return c.json(
-                { message: "User has posted too many times today." },
-                404
-            );
-        }
-    } catch (error) {
+    if (error) {
         return c.json(
             { message: "Could not validate post history.", error },
             400
         );
     }
 
-    try {
-        /**
-         * Create empty post object to save in DDB
-         * for other images to be attached via pre-signed URL
-         * upload.
-         */
-        const postId = await createEmptyPost(username);
+    if (data.Count && data.Count > DAILY_POST_LIMIT) {
+        return c.json(
+            { message: "User has posted too many times today." },
+            404
+        );
+    }
 
-        /**
-         * Generate pre-signed URLs for primary and secondary
-         * post images.
-         */
-        const keyPrefix = `images/${username}/posts`;
-        const [primaryUrl, secondaryUrl] = await generatePostPresignedUrls(
+    /**
+     * Create empty post object to save in DDB
+     * for other images to be attached via pre-signed URL
+     * upload.
+     */
+    const { data: postId, error: postError } = await tryCatch(
+        createEmptyPost(username)
+    );
+
+    if (postError) {
+        return c.json(
+            { message: "Failed to create empty post object.", postError },
+            400
+        );
+    }
+
+    /**
+     * Generate pre-signed URLs for primary and secondary
+     * post images.
+     */
+    const keyPrefix = `images/${username}/posts`;
+    const { data: s3Response, error: s3Error } = await tryCatch(
+        generatePostPresignedUrls(
             keyPrefix,
             postId,
             { width: params.width, height: params.height },
             { width: params.width, height: params.height }
-        );
+        )
+    );
 
-        return c.json({ primary: primaryUrl, secondary: secondaryUrl }, 200);
-    } catch (error) {
-        return c.json({ message: "Could not create post.", error }, 400);
+    if (s3Error) {
+        return c.json(
+            {
+                message: "Could not create upload location for post images.",
+                s3Error,
+            },
+            400
+        );
     }
+
+    const [primaryUrl, secondaryUrl] = s3Response;
+    return c.json({ primary: primaryUrl, secondary: secondaryUrl }, 200);
 });
 
 export const handler = handle(app);
